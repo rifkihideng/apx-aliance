4951d8e313fc03c8b12ece3af807093884b25d90
rifkihideng <ardiyansahrifkiar@gmail.com>
2026-09-09 21:55:54 +0700

    add libsql lib

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["better-sqlite3"],
+  serverExternalPackages: ["better-sqlite3", "@libsql/client"],
 };
 
 export default nextConfig;
